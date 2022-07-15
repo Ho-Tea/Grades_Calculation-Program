7b11d407617f7b762af74e90459594eae7db8498
yoonjuho <dbswn990@naver.com>
2022-07-15 23:33:55 +0900

    feat: 학생클래스 구성
    학생클래스를 구성하여 따른 클래스를 이용해 관리

--- src/src/Student.java
+++ src/src/Student.java
@@ -1,7 +1,46 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Student {
     private int sid;
     private String name;
-    private String major;
+    Major major;
+    HashMap<String, Score> score;
+    Subject subject;
+
+    Student(int sid, String name, String major){
+        this.sid = sid;
+        this.name = name;
+        divisionMajor(major);
+        score = new HashMap<>();
+    }
+
+    void divisionMajor(String major){
+        if(major.equals("국어국문학과"))
+            this.major = new LanguageMajor();
+        else if (major.equals("컴퓨터공학과"))
+            this.major = new ComputerMajor();
+        else
+            System.out.println("전공의 이름이 맞지 않습니다");
+    }
+
+    void enterScore(int math, int korean){
+        if(major.getVitalSubject().equals(subject.getMath())) {
+            score.put(major.getVitalSubject(), new VitalScore(math));
+            score.put(major.getNormalSubject(),new NormalScore(korean));
+        }
+        else if (major.getVitalSubject().equals(subject.getKorean())){
+            score.put(major.getVitalSubject(), new VitalScore(korean));
+            score.put(major.getNormalSubject(),new NormalScore(math));
+    }
+        }
+
+    void printGrade(){
+        for(Map.Entry<String,Score> map: score.entrySet())
+            System.out.println(map.getKey() + " : " + map.getValue().getGrade() );
+    }
 
 
 }
--- src/src/Subject.java
+++ src/src/Subject.java
@@ -1,2 +1,11 @@
 public class Subject {
+    private String math = "수학";
+    private String korean = "국어";
+
+    public String getMath(){
+        return math;
+    }
+    public String getKorean(){
+        return korean;
+    }
 }
